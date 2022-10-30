724f50388118ad1baef0077d5653f54cd3872cfa
netanel <netanelha@post.jce.ac.il>
2022-10-30 16:16:23 +0200

    frontend
    front end

---
+++ frontend_react/src/components/SocialMedia.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import { BsTwitter, BsInstagram } from 'react-icons/bs';
+import { FaFacebookF } from 'react-icons/fa';
+
+const SocialMedia = () => (
+  <div className="app__social">
+    {/* <div>
+      <FaFacebookF />
+    </div>
+    <div>
+      <BsInstagram />
+    </div> */}
+  </div>
+);
+
+export default SocialMedia;
